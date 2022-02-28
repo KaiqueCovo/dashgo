40ed475df09d7ad20d64e8c66fcf1bfcb85bf441
Kaique Covo <kaique.kng@gmail.com>
2022-02-28 13:52:18 -0300

    feat(tsconfig/input/pages): add input component, sign in page and absolut paths

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,22 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react"
+
+interface IInputProps extends ChakraInputProps {
+  name: string;
+  label?: string
+}
+
+export const Input = ({ name, label, ...resProps }: IInputProps) => (
+  <FormControl>
+    {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+    <ChakraInput
+      id={name}
+      name={name}
+      focusBorderColor="pink.500"
+      bgColor="gray.900"
+      variant="filled"
+      _hover={{ bgColor: ' gray.900' }}
+      size="lg"
+      {...resProps}
+    />
+  </FormControl>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,48 @@
+
 import type { NextPage } from 'next'
-const Home: NextPage = () => {
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '@/components/Form/Input'
+
+const SignIn: NextPage = () => {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input 
+            label="E-mail"
+            type="email"
+            name="email"
+          />
+          <Input 
+            label="Senha"
+            type="password"
+            name="password"
+          />
+        </Stack>
+        <Button 
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
