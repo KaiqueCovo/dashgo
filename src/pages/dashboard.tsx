99b60273b4d0af9028e3371b7872ca6579c5517c
Kaique Covo <kaique.kng@gmail.com>
2022-03-01 10:41:36 -0300

    feat(sidebar): add sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,18 @@
 import { Header } from '@/components/Header';
+import { Sidebar } from '@/components/Sidebar';
+import { Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Dashboard: NextPage = () => {
-  return (<Header />);
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
