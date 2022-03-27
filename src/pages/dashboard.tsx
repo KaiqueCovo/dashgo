82122c9c9a02eff8e4037613a5af891d0454ec84
Kaique Covo <kaique.kng@gmail.com>
2022-03-27 15:27:36 -0300

    refactor: responsive box dashboad

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -71,7 +71,7 @@ const Dashboard: NextPage = () => {
 
         <SimpleGrid flex={1} gap={4} minChildWidth="320px" alignItems="flex-start">
           <Box
-            p="8"
+            p={{ base: '6', md: '8' }}
             bg="gray.800"
             borderRadius={8}
             pb="4"
